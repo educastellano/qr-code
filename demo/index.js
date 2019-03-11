56918ac1ff651ba877e08d26f5585860f53e05c8
Eduard Castellano <educastellano08@gmail.com>
2019-03-11 13:05:27 +0100

    Replace Browserify for Webpack

--- demo/index.js
+++
@@ -1,6 +0,0 @@
-import '..'
-
-// Custom name
-// 
-// import QRCode from '../qr-code'
-// customElements.define('myapp-qrcode', QRCode)
---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.bundle.js'
+  }
+};
